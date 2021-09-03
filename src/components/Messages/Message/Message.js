443b7a522c0772346c144c42a98b9453c5a651ab
Michael Carr <xyeres@me.com>
2021-09-03 16:21:11 -0700

    Adds time stubs

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -3,14 +3,13 @@ import ReactEmoji from 'react-emoji';
 
 import profilePic from '../../../img/profiles/building.png'
 
-const Message = ({ getProfilePic, message: { user, text, picIndex }, name }) => {
+const Message = ({ time, getProfilePic, message: { user, text, picIndex }, name }) => {
   // let isSentByCurrentUser = false;
   let isSentByAdmin = false;
 
   // const trimmedName = name.trim().toLowerCase();
   // if (user === trimmedName) isSentByCurrentUser = true;
   if (user === 'admin') isSentByAdmin = true;
-
 
   return (
     isSentByAdmin
@@ -19,19 +18,19 @@ const Message = ({ getProfilePic, message: { user, text, picIndex }, name }) => 
           <img src={profilePic} alt="user #231" className="message__userImage" />
           <div className="message__detail">
             <div className="message__header">
-              <div className="message__userName">{'✨ChatBot: ' + user}</div>
-              <div className="message__timestamp">07/14/2021</div>
+              <div className="message__userName">{'✨ChatBot'}</div>
+              {/* <div className="message__timestamp">{time}</div> */}
             </div>
             <div className="message__text">{ReactEmoji.emojify(text)}</div>
           </div>
         </div>
       ) : (
         <div className="message">
-          <img src={getProfilePic(picIndex)} alt="user #231" className="message__userImage" />
+          <img src={getProfilePic(picIndex)} alt={`user #${picIndex}`} className="message__userImage" />
           <div className="message__detail">
             <div className="message__header">
-              <div className="message__userName">{user}</div>
-              <div className="message__timestamp">07/14/2021</div>
+              <div className="message__userName">{name}</div>
+              {/* <div className="message__timestamp">{time}</div> */}
             </div>
             <div className="message__text">{ReactEmoji.emojify(text)}</div>
           </div>
