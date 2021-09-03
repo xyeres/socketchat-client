37e6f8ba2e45e9d8009bd56311908ecf31506692
Michael Carr <xyeres@me.com>
2021-09-03 16:20:17 -0700

    Remove console log

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,13 +7,11 @@ import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
 import UserMenu from '../UserMenu/UserMenu';
 
-const DEBUG = 0
-
-
+const DEBUG = 1
 let socket;
 
 const Chat = ({ location }) => {
-  const [name, setName] = useState('Anonymous');
+  const [name, setName] = useState('');
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
   const [room, setRoom] = useState('');
@@ -35,6 +33,7 @@ const Chat = ({ location }) => {
     const { room, name } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
+
 
     setName(name);
     setRoom(room);
@@ -64,12 +63,10 @@ const Chat = ({ location }) => {
     socket.on('roomData', ({ users }) => {
       setUsers(users)
     })
-    console.log(users)
   }, [users])
 
   const sendMessage = (event) => {
     event.preventDefault();
-
     if (message) {
       socket.emit('sendMessage', message, () => setMessage(''))
     }
