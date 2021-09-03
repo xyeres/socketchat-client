cd1371cd44981febe8a9e4c35cbcedde2353c1bc
Michael Carr <xyeres@me.com>
2021-09-03 16:21:26 -0700

    fix if (index !== undefined) bug

--- src/modules/getRandomProfilePic.js
+++ src/modules/getRandomProfilePic.js
@@ -1,16 +1,15 @@
-import p0 from '../img/profiles/building.png'
-import p1 from '../img/profiles/face.png'
-import p2 from '../img/profiles/ghost.png'
-import p3 from '../img/profiles/map.png'
-import p4 from '../img/profiles/owl.png'
-import p5 from '../img/profiles/peacock.png'
-import p6 from '../img/profiles/plant.png'
-import p7 from '../img/profiles/squid.png'
+import p0 from '../img/profiles/building.png';
+import p1 from '../img/profiles/face.png';
+import p2 from '../img/profiles/ghost.png';
+import p3 from '../img/profiles/map.png';
+import p4 from '../img/profiles/owl.png';
+import p5 from '../img/profiles/peacock.png';
+import p6 from '../img/profiles/plant.png';
+import p7 from '../img/profiles/squid.png';
 
 function getRandomProfilePic(index) {
   let profilePics = [p0, p1, p2, p3, p4, p5, p5, p6, p7];
-
-  if (index) {
+  if (index !== undefined) {
     return profilePics[index]
   }
 
